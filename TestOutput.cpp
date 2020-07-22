b85cb08e51956c6f2269ac1804c8b1733104dcfd
Andrea Mugnaini <67502973+andreunifi@users.noreply.github.com>
2020-07-22 13:06:00 +0200

    Added Test Output

---
+++ TestOutput.cpp
@@ -0,0 +1,56 @@
+PASS	Executing test case CalendarTests
+	Qt version: 5.15.0
+	Qt build: Qt 5.15.0 (i386-little_endian-ilp32 shared (dynamic) release build; by GCC 8.1.0)
+	QTest version: 5.15.0
+PASS	Executing test function initTestCase
+PASS	CalendarTests::initTestCase
+	Execution took 4.489 ms.
+PASS	Executing test function WrongDateinCTor
+PASS	CalendarTests::WrongDateinCTor
+	Execution took 3.6268 ms.
+PASS	Executing test function setWrongTimeOfDay
+PASS	CalendarTests::setWrongTimeOfDay
+	Execution took 0.0139 ms.
+PASS	Executing test function setWrongDate
+PASS	CalendarTests::setWrongDate
+	Execution took 0.0105 ms.
+PASS	Executing test function setWrongDateANDTime
+PASS	CalendarTests::setWrongDateANDTime
+	Execution took 0.0166 ms.
+PASS	Executing test function setRightDate
+PASS	CalendarTests::setRightDate
+	Execution took 0.0114 ms.
+PASS	Executing test function serRightDateANDTime
+PASS	CalendarTests::serRightDateANDTime
+	Execution took 0.0196 ms.
+PASS	Executing test function CompareDates
+PASS	CalendarTests::CompareDates
+	Execution took 0.0086 ms.
+PASS	Executing test function CompareTimes
+PASS	CalendarTests::CompareTimes
+	Execution took 0.008 ms.
+PASS	Executing test function StartTimeGreaterEndTimeinCTor
+PASS	CalendarTests::StartTimeGreaterEndTimeinCTor
+	Execution took 0.0161 ms.
+PASS	Executing test function StartTimeGreaterEndTimewithSetTime
+PASS	CalendarTests::StartTimeGreaterEndTimewithSetTime
+	Execution took 0.0124 ms.
+PASS	Executing test function EndTimeLesserStartTimeinCTor
+PASS	CalendarTests::EndTimeLesserStartTimeinCTor
+	Execution took 0.0122 ms.
+PASS	Executing test function EndTimeLesserStartTimewithSetTime
+PASS	CalendarTests::EndTimeLesserStartTimewithSetTime
+	Execution took 0.0103 ms.
+PASS	Executing test function AddActivity
+PASS	CalendarTests::AddActivity
+	Execution took 0.0347 ms.
+PASS	Executing test function EditActivityNoDayFound
+PASS	CalendarTests::EditActivityNoDayFound
+	Execution took 0.0158 ms.
+PASS	Executing test function DeleteActivityNoDayFound
+PASS	CalendarTests::DeleteActivityNoDayFound
+	Execution took 0.0162 ms.
+PASS	Executing test function cleanupTestCase
+PASS	CalendarTests::cleanupTestCase
+	Execution took 0.0055 ms.
+	Test execution took 8.6362 ms.
